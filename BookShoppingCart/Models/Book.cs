01f6739e6cf441f263062c3532eae99eda302f59
gnart1 <76728890+gnart1@users.noreply.github.com>
2023-10-09 23:45:03 +0700

    Add project files.

---
+++ BookShoppingCart/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShoppingCart.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(32)]
+        public string BookName { get; set; }
+        [MaxLength(128)]
+    }
+}
